535018cd38cd453b90467e13baf5820680bfc27e
OR <oreyesc@gmail.com>
2018-04-14 17:00:54 -0500

    updated js

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,36 @@
 $(function() {
-
   // your code will go here
-
+  $.ajax( {
+    url: 'https://www.codeschool.com/users/or73.json',
+    dataType: 'jsonp',
+    success: function ( response ) {
+      // console.log( 'response: ', response);
+      addCourses( response.courses.completed );
+    }
+  } );
+  
+  function addCourses( courses ) {
+    let $badges = $( '#badges' );
+    
+    courses.forEach( function ( course ) {
+      let $course = $( '<div />', {
+        'class': 'course'
+      } ).appendTo( $badges );
+      
+      $( '<h3 />', {
+        text: course.title
+      }).appendTo( $course );
+      
+      $('<img />', {
+        src: course.badge
+      }).appendTo( $course );
+      
+      $( '<a />', {
+        'class': 'btn btn-primary',
+        target: '_black',
+        href: course.url,
+        text: 'See Course'
+      }).appendTo( $course );
+    });
+  }
 });
